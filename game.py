e8eb5c41162faf3e051ee0a75ed663741c1413d4
vagrant <vagrant@vagrant.vm>
2020-09-29 23:44:44 +0000

    while loop to eval guess

--- game.py
+++ game.py
@@ -1,9 +1,28 @@
 """A number-guessing game."""
-
+#import random function randint
 from random import randint
 
 print("Welcome to the Guessing Game!")
-
 name = input("What is your name? ")
-
 randnum = randint(1, 100)
+game_active = True
+num_guesses = 0
+
+#repeat forever:
+#create while loop
+while game_active == True:
+    #ask for guess
+    playernum = int(input("Guess a number: "))
+    #check if guess is correct
+    if playernum != randnum:
+        num_guesses += 1
+        if playernum > randnum:
+            print("Your guess is too high.")
+        else:
+            print("Your guess is too low.")
+    #if guess is incorrect:
+        #give hint and increment number of guesses
+    #if guess is correct
+    else:
+        game_active = False
+        #congratulate player
