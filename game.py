5509f443dea2a502be42dc492bc5a3bd94018092
vagrant <vagrant@vagrant.vm>
2020-09-30 00:47:02 +0000

    allowed user to play again

--- game.py
+++ game.py
@@ -31,14 +31,28 @@ while game_active == True:
                 num_guesses += 1
                  #congratulate player
                 print(f"Well done, {name}! You've found my number in {num_guesses} amount of valid and invalid tries!")
-                 #after a round, ask player if they want to play again
-                 #if player wants to play again:
-                    #reset random number
-                    #reset number of tries/num_guesses
-                #if player doesn't want to play again:
-                    #turn off game
-                    #game_active = False
-                    #Tell user how many times they played and what their best score is.
+                askloop = True
+                while askloop == True:
+                    play_again = input("Do you want to play again?(y/n) ")
+                    #after a round, ask player if they want to play again
+                    #if player wants to play again:
+                        #reset random number
+                        #reset number of tries/num_guesses
+                    if play_again == "y":
+                        randnum = randint(1, 100)
+                        num_guesses = 0
+                        askloop = False
+                    #if player doesn't want to play again:
+                        #turn off game
+                        #game_active = False
+                        #Tell user how many times they played and what their best score is.
+                    elif play_again == "n":
+                        game_active = False
+                        askloop = False
+                        print(f"You've made it to play_again = n")
+                    else:
+                        print("That's not a valid response! Please enter y or n.")
+
     except ValueError:
         num_guesses += 1
         print("That's not a valid number. Please put an integer.")
